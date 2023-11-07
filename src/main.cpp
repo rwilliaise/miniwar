0bf3d72bb7b58394868f90c82afb79a6117cf365
rwilliaise <12103838+rwilliaise@users.noreply.github.com>
2023-11-06 20:25:36 -0800

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_video.h>
+
+int main(int argc, char *argv[]) {
+    SDL_Window *win = SDL_CreateWindow("miniwar", 0, 0, 640, 480, SDL_WINDOW_OPENGL);
+
+    return 0;
+}
